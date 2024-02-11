dce44cfc912304354429eb33fd785231b6aca7d3
Anderson Silva <andersonsoufeliz27@gmail.com>
2024-02-11 18:16:42 -0300

    aula 42 parte2

---
+++ aula43script.js
@@ -0,0 +1,33 @@
+// const idades = [15,21,30,17,18,44,12,50]
+
+// const filtroMaior18 =((valor, index , array)=>{
+//   if (valor >= 18)
+//   return valor;
+// })
+// const filtroMenor18 = ((valor,index,array)=>{
+//   if(valor < 18)
+//   return valor
+// })
+// const idades = [15, 21, 30, 17, 18, 44, 12, 50];
+
+// const maior = idades.filter((valor, index, array) => {
+//   if (valor >= 18) return valor;
+// });
+// const menor = idades.filter((valor, index, array) => {
+//   if (valor < 18) return valor;
+// });
+// console.log(idades);
+// console.log(maior);
+// console.log(menor);
+
+
+// const terceiraIdade = maior.filter((valor,index,array)=>{
+//  if(valor >40)
+//  return valor
+// })
+// const par = terceiraIdade.filter((valor,index,array)=>{
+//   if (valor % 2 === 0)
+//   return valor
+// })
+// console.log(terceiraIdade)
+// console.log(par)
